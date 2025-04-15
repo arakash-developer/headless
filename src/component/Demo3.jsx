7c00ca22eecf41b3464d0805db03e41f08b48990
akashblinto <akash.blinto@gmail.com>
2025-04-15 14:12:11 +0600

    Remove unused state and handler from Demo3; simplify component structure

--- src/component/Demo3.jsx
+++ src/component/Demo3.jsx
@@ -1,32 +1,9 @@
-import React, { useState } from 'react'
-import postRegistration from '../lib/postRegistration'
+import React from 'react'
 
 const Demo3 = () => {
-    const [formData, setFormData] = useState({
-      username: "as",
-      password: "a",
-      email: "aasaa@gmail.com",
-      firstName: "s",
-      lastName: "s",
-      title: "s",
-      company: "s",
-      phone: "s",
-      extension: "s",
-      code: "s",
-    });
-
-
-
-  let handlerData = async () => {
-    let response = await postRegistration(formData);
-    console.log(response);
-  }
-
   return (
-
     <div>
-      <h1>dd</h1>
-      <p className='py-5 px-10 bg-red-400' onClick={handlerData}>GG</p>
+      
     </div>
   )
 }
