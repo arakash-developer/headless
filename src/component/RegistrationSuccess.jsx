c55d8fb3c2595d5063c93896706334bba0860f8d
akashblinto <akash.blinto@gmail.com>
2025-04-16 09:48:09 +0600

    Refactor RegistrationSuccess component; update styling for success message and improve code formatting

--- src/component/RegistrationSuccess.jsx
+++ src/component/RegistrationSuccess.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaCheck } from 'react-icons/fa'
+import React from "react";
+import { FaCheck } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const RegistrationSuccess = () => {
   return (
     <>
       <div className="p-[60px] bg-[#E9E9E9] max-w-[800px]">
-        <div className="w-[75px] h-[75px] bg-[#080607] mx-auto mb-10 rounded-full flex justify-center items-center">
-          <FaCheck className='text-[#FFF] text-[32px] not-italic font-semibold leading-10' />
+        <div className="w-[75px] h-[75px] bg-[#ED272C] mx-auto mb-10 rounded-full flex justify-center items-center">
+          <FaCheck className="text-[#FFF] text-[32px] not-italic font-semibold leading-10" />
         </div>
-        <h2 className='text-[#000] text-[32px] not-italic font-bold leading-10 text-center'>Registration Successful!</h2>
-        <p className='pt-6 pb-10 text-[#000] text-center text-[16px] not-italic font-normal leading-6 max-w-[680px]'>Your account has been successfully created. You can now log in to access your dashboard and get started!</p>
+        <h2 className="text-[#000] text-[32px] not-italic font-bold leading-10 text-center">
+          Registration Successful!
+        </h2>
+        <p className="pt-6 pb-10 text-[#000] text-center text-[16px] not-italic font-normal leading-6 max-w-[680px]">
+          Your account has been successfully created. You can now log in to
+          access your dashboard and get started!
+        </p>
         <div className="w-full flex justify-center">
-
-        <Link to="/login" className='bg-[#ED272C] py-[18px] px-[60px] text-[#fff] text-[16px] not-italic font-medium leading-6 text-center rounded-[5px] text-[#FFF] text-[16px] not-italic font-semibold leading-7'>Go to Log In</Link>
+          <Link
+            to="/login"
+            className="bg-[#ED272C] py-[18px] px-[60px] text-[#fff] text-[16px] not-italic text-center rounded-[5px]  font-semibold leading-7"
+          >
+            Go to Log In
+          </Link>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default RegistrationSuccess
+export default RegistrationSuccess;
