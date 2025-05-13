496194d6ee47af02f7ec1b3c1cfae6a47bd9f0da
akashblinto <akash.blinto@gmail.com>
2025-05-13 16:48:47 +0600

    style: update tooltip title casing for consistency in Services component

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -133,7 +133,7 @@ const Services = () => {
                     </Tooltip>
                   </td>
                   <td className="px-6 py-2 border-b-2 border-r-2">
-                    <Tooltip title="Paid hourly">
+                    <Tooltip title="Paid Hourly">
                       <Button
                         style={{
                           backgroundColor: "transparent",
