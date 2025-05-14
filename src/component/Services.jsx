cf513dea2d9f9a81eb219526af90f3d420f592d7
akashblinto <akash.blinto@gmail.com>
2025-05-14 09:58:59 +0600

    feat: enhance Customer Information form layout with additional input fields

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -155,12 +155,24 @@ const Services = () => {
           </table>
         </div>
       </div>
-      <div className="pt-3 pb-[33px] pl-5 mt-6 bg-[var(--secondary)] servicecard rounded-[8px]">
+      <div className="pt-3 pb-[33px] px-5 mt-6 bg-[var(--secondary)] servicecard rounded-[8px]">
         <h3 className="font-semibold text-lg leading-[156%] text-[#343a40]">
           Customer Information
         </h3>
-        <form className="mt-3">
-          <div className="flex flex-col gap-y-[2px]">
+        <form className="mt-3 flex gap-x-6 items-center justify-between">
+          <div className="w-1/2 flex flex-col gap-y-[2px]">
+            <label htmlFor="" className="font-medium text-sm leading-[171%] text-[var(--text-secondary)]">Lessee No</label>
+            <input type="text" className='font-normal text-sm leading-[171%] text-[var(--text-disabled)] py-2 px-3 border border-[var(--neutral-400)] rounded-[8px] outline-none' placeholder='L-1001'/>
+          </div>
+          <div className="w-1/2 flex flex-col gap-y-[2px]">
+            <label htmlFor="" className="font-medium text-sm leading-[171%] text-[var(--text-secondary)]">Lessee No</label>
+            <input type="text" className='font-normal text-sm leading-[171%] text-[var(--text-disabled)] py-2 px-3 border border-[var(--neutral-400)] rounded-[8px] outline-none' placeholder='L-1001'/>
+          </div>
+          <div className="w-1/2 flex flex-col gap-y-[2px]">
+            <label htmlFor="" className="font-medium text-sm leading-[171%] text-[var(--text-secondary)]">Lessee No</label>
+            <input type="text" className='font-normal text-sm leading-[171%] text-[var(--text-disabled)] py-2 px-3 border border-[var(--neutral-400)] rounded-[8px] outline-none' placeholder='L-1001'/>
+          </div>
+          <div className="w-1/2 flex flex-col gap-y-[2px]">
             <label htmlFor="" className="font-medium text-sm leading-[171%] text-[var(--text-secondary)]">Lessee No</label>
             <input type="text" className='font-normal text-sm leading-[171%] text-[var(--text-disabled)] py-2 px-3 border border-[var(--neutral-400)] rounded-[8px] outline-none' placeholder='L-1001'/>
           </div>
