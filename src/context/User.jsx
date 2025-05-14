158ee9cc546d5fe8c934e406c206cdc3406036cc
akash100a <arakash968786@gmail.com>
2025-05-14 20:24:21 +0600

    Import Ant Design CSS in main.jsx for consistent styling across components

--- src/context/User.jsx
+++ src/context/User.jsx
@@ -3,11 +3,12 @@ import { createContext, useState } from "react";
 export let Contex = createContext();
 
 let User = ({ children }) => {
+  let [assetTerm, setAssetTerm] = useState(false);
   let [invitation, setInvitation] = useState({});
   let [islogin, setIsLogin] = useState(false);
 
   return (
-    <Contex.Provider value={{ invitation, setInvitation, islogin, setIsLogin }}>
+    <Contex.Provider value={{ assetTerm, setAssetTerm,invitation, setInvitation, islogin, setIsLogin }}>
       {children}
     </Contex.Provider>
   );
