a6a82b7adfcf1e8d2881c8867f4a5efd58d7f75e
akashblinto <akash.blinto@gmail.com>
2025-05-15 15:34:14 +0600

    style: Refactor layout structure in Login component for improved clarity and consistency

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -106,72 +106,70 @@ const Login = () => {
 
   return (
     <>
-      <div className="h-full w-full">
-        <div className="loginBox bg-[#fff]  mr-[80px] mb-[171px] rounded-[5px] px-[125px] py-5 flex gap-[80px] h-full items-center">
-          <div className="max-w-[380px]">
-            <h3 className="text-[var(--primary2)] text-4xl not-italic font-bold leading-10">
-              Log In
-            </h3>
-            <p className="text-[#919191] text-base not-italic font-normal leading-6 mt-[8px] mb-[28px]">
-              Good to see you again! Enter your details to continue using the
-              dashboard
-            </p>
-            <form onSubmit={handleLogin}>
-              <div className="flex flex-col gap-y-[20px]">
-                <div className="flex flex-col gap-y-2">
-                  <label
-                    htmlFor="email"
-                    className="text-[var(--primary2)] text-base not-italic font-medium leading-[normal]"
-                  >
-                    Email
-                  </label>
-                  <input
-                    id="email"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="w-full h-[50px] py-3 px-4 border-[1.4px] border-[#DBDCDE] rounded-[8px] focus:outline-none focus:ring-0 placeholder:text-[#919191] placeholder:text-sm placeholder:not-italic placeholder:font-normal placeholder:leading-[normal]"
-                    required
-                  />
-                </div>
-                <div className="flex flex-col gap-y-2">
-                  <label
-                    htmlFor="password"
-                    className="text-[var(--primary2)] text-base not-italic font-medium leading-[normal]"
-                  >
-                    Password
-                  </label>
-                  <input
-                    id="password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="w-full h-[50px] py-3 px-4 border-[1.4px] border-[#DBDCDE] rounded-[8px] focus:outline-none focus:ring-0 placeholder:text-[#919191] placeholder:text-sm placeholder:not-italic placeholder:font-normal placeholder:leading-[normal]"
-                    required
-                  />
-                </div>
+      <div className="loginBox bg-[#fff]  mr-[80px] mb-[171px] rounded-[5px] px-[125px] py-5 flex gap-[80px] h-full items-center">
+        <div className="max-w-[380px]">
+          <h3 className="text-[var(--primary2)] text-4xl not-italic font-bold leading-10">
+            Log In
+          </h3>
+          <p className="text-[#919191] text-base not-italic font-normal leading-6 mt-[8px] mb-[28px]">
+            Good to see you again! Enter your details to continue using the
+            dashboard
+          </p>
+          <form onSubmit={handleLogin}>
+            <div className="flex flex-col gap-y-[20px]">
+              <div className="flex flex-col gap-y-2">
+                <label
+                  htmlFor="email"
+                  className="text-[var(--primary2)] text-base not-italic font-medium leading-[normal]"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full h-[50px] py-3 px-4 border-[1.4px] border-[#DBDCDE] rounded-[8px] focus:outline-none focus:ring-0 placeholder:text-[#919191] placeholder:text-sm placeholder:not-italic placeholder:font-normal placeholder:leading-[normal]"
+                  required
+                />
               </div>
-              <div className="mt-2 mb-6 flex items-center gap-3 cursor-pointer">
-                <input type="checkbox" />
-                <p className="text-[#919191] text-sm font-normal">
-                  Remember Password
-                </p>
+              <div className="flex flex-col gap-y-2">
+                <label
+                  htmlFor="password"
+                  className="text-[var(--primary2)] text-base not-italic font-medium leading-[normal]"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full h-[50px] py-3 px-4 border-[1.4px] border-[#DBDCDE] rounded-[8px] focus:outline-none focus:ring-0 placeholder:text-[#919191] placeholder:text-sm placeholder:not-italic placeholder:font-normal placeholder:leading-[normal]"
+                  required
+                />
               </div>
-              <button
-                type="submit"
-                className="py-[18px] px-[60px] bg-[var(--primary)] rounded-[5px] text-white text-base font-bold max-w-[192px] cursor-pointer"
-              >
-                Sign In
-              </button>
-            </form>
-          </div>
-          <div className="">
-            <img
-              className="bg-cover rounded-[5px]"
-              src={Loginbanner}
-              alt="Login Banner"
-            />
-          </div>
+            </div>
+            <div className="mt-2 mb-6 flex items-center gap-3 cursor-pointer">
+              <input type="checkbox" />
+              <p className="text-[#919191] text-sm font-normal">
+                Remember Password
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="py-[18px] px-[60px] bg-[var(--primary)] rounded-[5px] text-white text-base font-bold max-w-[192px] cursor-pointer"
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+        <div className="">
+          <img
+            className="bg-cover rounded-[5px]"
+            src={Loginbanner}
+            alt="Login Banner"
+          />
         </div>
       </div>
     </>
