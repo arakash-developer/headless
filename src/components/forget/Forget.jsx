b5e44c03fd87fafa67b2d426f43b949cf39d550c
akashblinto <akash.blinto@gmail.com>
2025-05-20 16:34:39 +0600

    fix: refactor Forget component to implement step navigation and improve layout

--- src/components/forget/Forget.jsx
+++ src/components/forget/Forget.jsx
@@ -1,8 +1,42 @@
 import emailjs from "@emailjs/browser";
 import ForgetIcon from "@public/forget.png";
-import { Input } from "antd";
+import { Input, theme } from "antd";
 import { useRef, useState } from "react";
 
+const ForgetDesign = () => {
+  const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  return (
+    <>
+      <h2 className="font-semibold text-[32px] leading-[125%] text-[var(--primary2)]">
+        Forgot Password?
+      </h2>
+      <p className="font-normal text-base leading-[150%] text-[var(--text-secondary)] mt-2 mb-[28px]">
+        Enter your registered email address to receive a password reset link
+      </p>
+    </>
+  );
+};
+
+const steps = [
+  {
+    title: "First",
+    content: <ForgetDesign />,
+  },
+  {
+    title: "Second",
+    content: "Second-content",
+  },
+  {
+    title: "Last",
+    content: "Last-content",
+  },
+];
 const Forget = () => {
   const form = useRef();
   const [status, setStatus] = useState(""); // For showing messages
@@ -25,32 +59,78 @@ const Forget = () => {
         }
       );
   };
-
+  const { token } = theme.useToken();
+  const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+  const contentStyle = {
+    lineHeight: "260px",
+    textAlign: "center",
+    color: token.colorTextTertiary,
+    backgroundColor: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: `1px dashed ${token.colorBorder}`,
+    marginTop: 16,
+  };
   return (
     <>
+      {/* <Steps current={current} items={items} /> */}
       <div className="flex justify-between items-start loginBox mt-10 rounded-[8px] pt-5 pb-5 pr-4 max-w-[1099px] xl:mr-[80px] mb-[171px] bg-[var(--secondary)]">
         <div className="pl-[55px] py-[104px] w-[415px]">
-          <h2 className="font-semibold text-[32px] leading-[125%] text-[var(--primary2)]">
-            Forgot Password?
-          </h2>
-          <p className="font-normal text-base leading-[150%] text-[var(--text-secondary)] mt-2 mb-[28px]">
-            Enter your registered email address to receive a password reset link
-          </p>
-          <div className="">
-            <label
-              htmlFor=""
-              className="font-medium text-sm leading-[171%] text-[var(--text-normal)]"
-            >
-              Email
-            </label>
-            <Input className="custom-black-input font-normal text-sm leading-[171%] text-[var(--text-disabled)] py-2 px-3 border border-[var(--neutral-400)] rounded-[8px]" />
-            <input className="mt-8 cursor-pointer font-medium text-sm leading-[200%] text-[var(--secondary)] bg-[var(--primary)] py-2 px-8 rounded-[8px]" type="submit" value="Send Code" />
+          <div>
+            {steps[current].content}
+            <div className="">
+              <label
+                htmlFor=""
+                className="font-medium text-sm leading-[171%] text-[var(--text-normal)]"
+              >
+                Email
+              </label>
+              <Input className="custom-black-input font-normal text-sm leading-[171%] text-[var(--text-disabled)] py-2 px-3 border border-[var(--neutral-400)] rounded-[8px]" />
+              {current < steps.length - 1 && (
+                <input
+                  onClick={() => next()}
+                  className="mt-8 cursor-pointer font-medium text-sm leading-[200%] text-[var(--secondary)] bg-[var(--primary)] py-2 px-8 rounded-[8px]"
+                  type="submit"
+                  value="Send Code"
+                />
+              )}
+            </div>
           </div>
         </div>
         <div className="px-8">
           <img className="w-[475px] h-[408px]" src={ForgetIcon} alt="forget" />
         </div>
       </div>
+
+      <div>
+        {/* {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )} */}
+        {/* {current === steps.length - 1 && (
+          <Button
+            type="primary"
+            onClick={() => message.success("Processing complete!")}
+          >
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
+            Previous
+          </Button>
+        )} */}
+      </div>
+      {/* 
+
+*/}
 
       <form ref={form} onSubmit={sendEmail} className="hidden">
         <label>Name</label>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -142,6 +142,7 @@ const Login = () => {
                   className="custom-black-input w-full h-[40px] border-[1.4px] border-[#DBDCDE] rounded-[8px] focus:outline-none focus:ring-0 placeholder:text-[#919191] placeholder:text-sm placeholder:not-italic placeholder:font-normal placeholder:leading-[normal] py-3 px-4 bg-[var(--background)]"
                   required
                 />
+                
               </div>
               <div className="flex flex-col gap-y-2">
                 <label
