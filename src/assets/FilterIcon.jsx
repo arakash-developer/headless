42386c8497b4f1576754f9256a65dc1dd151f73c
akashblinto <akash.blinto@gmail.com>
2025-05-23 09:37:32 +0600

    Remove unnecessary fill attribute from FilterIcon component

--- src/assets/FilterIcon.jsx
+++ src/assets/FilterIcon.jsx
@@ -4,7 +4,6 @@ const FilterIcon = () => {
       width="10"
       height="10"
       viewBox="0 0 10 10"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
     >
