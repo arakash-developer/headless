f09804586889474cd02c523ba19eee5943fe48fd
akashblinto <akash.blinto@gmail.com>
2025-05-27 15:13:56 +0600

    fix: update navigation in ConfirmEmail and RouteWrappers components

--- src/components/registers/ConfirmEmail.jsx
+++ src/components/registers/ConfirmEmail.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const ConfirmEmail = () => {
   const [formData, setFormData] = useState({});
   const API_URL = "https://4amitest-bli6.wp1.sh/wp-json/users/v1";
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    navigate("/dashboard");
+  }, [navigate]);
 
-  return <div>ConfirmEmail</div>;
+  return null; 
 };
 
 export default ConfirmEmail;
--- src/routes/RouteWrappers.jsx
+++ src/routes/RouteWrappers.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("logintoken");
-  return token ? children : <Navigate to="/login" replace />;
+  return token ? children : <Navigate to="/" replace />;
 };
 
 export const GuestRoute = ({ children }) => {
