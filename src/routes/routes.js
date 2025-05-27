ae9b61e73c70967c224ada0ec1afb333c9effd6a
akashblinto <akash.blinto@gmail.com>
2025-05-27 14:33:56 +0600

    feat: add ConfirmEmail component for email confirmation functionality

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,12 +8,10 @@ import Forget from "@components/forget/Forget";
 import InviteUser from "@components/InviteUser";
 import Login from "@components/login/Login";
 import Register from "@components/registers/Register";
-import Register2 from "@components/registers/Register2";
 import RegistrationSuccess from "@components/registers/RegistrationSuccess";
 import Residualanalysis from "@components/residualanalysis/Residualanalysis";
 import Services from "@components/services/Services";
 
-import Alluser from '@components/demo/Alluser';
 import Demo from "@/components/demo/Demo";
 import Demo2 from "@/components/demo/Demo2";
 import Demo3 from "@/components/demo/Demo3";
@@ -21,6 +19,8 @@ import Demo4 from "@/components/demo/Demo4";
 import Demo5 from "@/components/demo/Demo5";
 import Demo6 from "@/components/demo/Demo6";
 import Demo7 from "@/components/demo/Demo7";
+import ConfirmEmail from "@/components/registers/ConfirmEmail";
+import Alluser from "@components/demo/Alluser";
 export const routes = [
   // public routes
   // { path: "*", component: Error, access: "public" },
@@ -28,10 +28,11 @@ export const routes = [
   // guest routes
   { path: "/demo7", component: Demo7, access: "guest" },
   { path: "/", component: Login, access: "guest" },
-  { path: "/register2", component: Register2, access: "guest" },
   { path: "/signup", component: Register, access: "guest" },
   { path: "/inviteuser", component: InviteUser, access: "guest" },
   { path: "/forget", component: Forget, access: "guest" },
+  { path: "/confirmemail", component: ConfirmEmail, access: "guest" },
+  // demo routes
   { path: "/alluser", component: Alluser, access: "guest" },
   { path: "/demo", component: Demo, access: "guest" },
   { path: "/demo2", component: Demo2, access: "guest" },
