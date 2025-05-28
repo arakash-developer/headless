7bf48a02f02bcbc58f7b195928c6e4a9156d5d7e
akashblinto <akash.blinto@gmail.com>
2025-05-28 12:58:10 +0600

    fix: correct ConfirmEmail component definition to remove async keyword

--- src/components/registers/ConfirmEmail.jsx
+++ src/components/registers/ConfirmEmail.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
-const ConfirmEmail = async () => {
+const ConfirmEmail =  () => {
   const { email } = useParams();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
