09fc3fd01882d9e19e995feb52378101b66cc7d9
akashblinto <arakash968786@gmail.com>
2025-06-20 14:37:28 +0600

    feat: implement ManageAssets component with enhanced UI and functionality

---
+++ src/components/manageassets/layers/CustomSort.jsx
@@ -0,0 +1,32 @@
+import { Select } from "antd";
+import { useState } from "react";
+
+const CustomSort = ({ option, defaultValue, color }) => {
+  const [selected, setSelected] = useState(defaultValue);
+
+  const handleChange = (value) => {
+    setSelected(value);
+    console.log("Selected:", value);
+  };
+
+  return (
+    <div className="w-[125px] flex flex-col gap-y-[2px]">
+      <Select
+        value={selected}
+        onChange={handleChange}
+        className="w-full font-normal text-xs leading-[135%] text-[#343a40]"
+        dropdownMatchSelectWidth={false}
+      >
+        {option.map((option) => (
+          <Select.Option key={option.value} value={option.value}>
+            <p className="font-normal text-xs leading-[135%] text-[#343a40]">
+              {option.label}
+            </p>
+          </Select.Option>
+        ))}
+      </Select>
+    </div>
+  );
+};
+
+export default CustomSort;
