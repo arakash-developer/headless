91c6fae8361937d7d93444cb87f23cd821a47b71
akashblinto <arakash968786@gmail.com>
2025-06-24 15:23:12 +0600

    feat: enhance GlobalProfile component with structured layout and navigation breadcrumbs

--- src/components/main/GlobalProfile.jsx
+++ src/components/main/GlobalProfile.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
 const GlobalProfile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <div className="mt-5">
+        <h2 className="font-medium text-[24px] leading-[117%] text-[#343a40]">
+          Profile
+        </h2>
+        <p className="font-normal text-base leading-[150%] text-[var(--gray)]">
+            Dashboard / <span className="ml-1 text-[#343a40]">Profile</span>{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default GlobalProfile
+export default GlobalProfile;
