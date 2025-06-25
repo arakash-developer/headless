9c3bdc5bb088562f664090e56bce4995f8f563e4
akashblinto <arakash968786@gmail.com>
2025-06-25 09:15:24 +0600

    feat: implement layout for AddAssets and AddServices components

--- src/components/admin/addassets/AddAssets.jsx
+++ src/components/admin/addassets/AddAssets.jsx
@@ -1,5 +1,20 @@
 const AddAssets = () => {
-  return <div>AddAssets</div>;
+  return (
+    <div>
+      <div className="mt-5">
+        <h1 className="font-semibold text-[32px] leading-[125%] text-[var(--primary2)]">
+          Add Assets
+        </h1>
+        <p className="mt-2 font-normal text-sm leading-[171%] text-[var(--gray)]">
+          Dashboard / Manage Assets / 
+          <span className="font-normal text-sm leading-[171%] text-[var(--primary2)]">
+            {" "}
+            Add Assets
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AddAssets;
--- src/components/admin/addservices/AddServices.jsx
+++ src/components/admin/addservices/AddServices.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
 const AddServices = () => {
   return (
-    <div>AddServices</div>
-  )
-}
+    <div>
+      <div className="mt-5">
+        <h1 className="font-semibold text-[32px] leading-[125%] text-[var(--primary2)]">
+          Add Services
+        </h1>
+        <p className="mt-2 font-normal text-sm leading-[171%] text-[var(--gray)]">
+          Dashboard / Manage Services /
+          <span className="font-normal text-sm leading-[171%] text-[var(--primary2)]">
+            {" "}
+            Add Services
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default AddServices
+export default AddServices;
